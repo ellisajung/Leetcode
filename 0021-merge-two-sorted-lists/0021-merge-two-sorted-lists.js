c4827aca60c4be6e7752b71f32c8174ee960759a
ellisajung <125180849+ellisajung@users.noreply.github.com>
2024-09-29 20:33:09 +0900

    Time: 84 ms (5.2%), Space: 52.5 MB (8.72%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+// approach:
+// create dummy node
+// iterating both lists
+// go comparing & appending the smaller value to the dummy node
+// after finishing iterating, append the rest of the remaining list
+// return the initial dummy node's next node
+
+// list1 = [1,2,4,5,6,7]
+//              l1
+// list2 = [1,3,4]
+//                l2
+// dummy-1-1-2-3-4-
+//               c
+var mergeTwoLists = function(list1, list2) {
+    const dummy = new ListNode()
+    let curr = dummy
+    
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            curr.next = list1
+            list1 = list1.next
+        } else {
+            curr.next = list2
+            list2 = list2.next
+        }
+        curr = curr.next
+    }
+    console.log(curr)
+    if (list1) {
+        curr.next = list1
+    } else {
+        curr.next = list2
+    }
+
+    return dummy.next
+};
