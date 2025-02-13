e163e7c7d0daa5767c89346335f7bc580e9e9595
Elisa <ellisajung12@gmail.com>
2025-02-13 16:28:34 +0900

    250213 스터디 - recursive DFS

--- DFS/0200-number-of-islands/0200-number-of-islands.js
+++ DFS/0200-number-of-islands/0200-number-of-islands.js
@@ -1,90 +1,34 @@
-// /**
-//  * @param {character[][]} grid
-//  * @return {number}
-//  */
-
-// var numIslands = function(grid) {
-//     let rows= grid.length, cols=grid[0].length
-
-//     function dfs(grid, r, c) {
-//         if (r<0 || c<0 || r>=rows || c>=cols || grid[r][c]==="0") {
-//             return
-//         }
-
-//         grid[r][c] = "0"
-
-//         dfs(grid, r+1, c)
-//         dfs(grid, r-1, c)
-//         dfs(grid, r, c+1)
-//         dfs(grid, r, c-1)
-//     }
-
-//     let count = 0
-//     for (let r=0; r<rows; r++) {
-//         for (let c=0; c<cols; c++) {
-//             if (grid[r][c]==="1") {
-//                 count++
-//                 dfs(grid, r, c)
-//             }
-//         }
-//     }
-
-//     return count
-// };
-
 /**
- * @param {number[][]} grid
+ * @param {character[][]} grid
  * @return {number}
  */
-var maxAreaOfIsland = function (grid) {
+
+var numIslands = function (grid) {
   let rows = grid.length,
     cols = grid[0].length;
 
   function dfs(grid, r, c) {
-    if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 0) {
+    if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === "0") {
       return;
     }
 
-    grid[r][c] = 0;
-    let count = 1;
+    grid[r][c] = "0";
 
-    count += dfs(grid, r + 1, c);
-    count += dfs(grid, r - 1, c);
-    count += dfs(grid, r, c + 1);
-    count += dfs(grid, r, c - 1);
-
-    console.log("count: ", count);
-    return count;
+    dfs(grid, r + 1, c);
+    dfs(grid, r - 1, c);
+    dfs(grid, r, c + 1);
+    dfs(grid, r, c - 1);
   }
 
-  let max = 0;
-  // let count = 0
+  let count = 0;
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < cols; c++) {
-      if (grid[r][c] === 1) {
-        console.log("r: ", r);
-        console.log("c: ", c);
-        let res = dfs(grid, r, c);
-        max = Math.max(res, max);
+      if (grid[r][c] === "1") {
+        count++;
+        dfs(grid, r, c);
       }
     }
   }
 
-  console.log(max);
-  return max;
+  return count;
 };
-
-console.log(
-  maxAreaOfIsland(
-    (grid = [
-      [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-      [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-      [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-    ]),
-  ),
-);
