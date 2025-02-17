6c26c8cac4c9aa3f07c27629586a3f43c652478a
Elisa <ellisajung12@gmail.com>
2025-02-17 17:58:45 +0900

    Time: 0 ms (100%), Space: 49.2 MB (7.88%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,31 @@
+/* 
+base case:
+    1. the sum of steps = n
+    2. the sum of steps > n
+decision tree: n=5
+            0
+       +1 /    \ +2
+         1      2
+       /    \     /   \
+      2      3    3    4
+    / \     / \  / \   / \
+   3  4    4  5 4 "5" "5" "6"
+  / \  / \
+ 4 "5" ...
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n <= 2) {
+        return n;
+    }
+    let dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
