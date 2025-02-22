8ccf465e1abd00b7363c338403e1b3b2ad3bc10e
Elisa <ellisajung12@gmail.com>
2025-02-22 15:56:23 +0900

    250222 스터디 - [Merge Sort] merge k sorted lists

--- 4.Sorting/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 4.Sorting/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -14,7 +14,7 @@
 //  dummy        \      l1
 //        \  [3 -> 4]
 //               l2
-//                c   
+//                c
 
 /**
  * Definition for singly-linked list.
@@ -27,63 +27,63 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-function mergeKLists (lists) {
-    // Edge cases
-    if (!lists || lists.length === 0) { // lists가 null이거나 빈 배열일 경우
-        return null
-    }
+function mergeKLists(lists) {
+  // Edge cases
+  if (!lists || lists.length === 0) {
+    // lists가 null이거나 빈 배열일 경우
+    return null;
+  }
 
-    return divide(lists, 0, lists.length - 1)
+  return divide(lists, 0, lists.length - 1);
 }
 
 /**
-* @param {ListNode[]} lists
-* @param {number} l
-* @param {number} r
-* @return {ListNode}
-*/
+ * @param {ListNode[]} lists
+ * @param {number} l
+ * @param {number} r
+ * @return {ListNode}
+ */
 function divide(lists, l, r) {
-    if (l > r) { // 이 조건은 왜 있어야 되는거임?
-        return null
-    }
+  if (l > r) {
+    // 이 조건은 왜 있어야 되는거임?
+    return null;
+  }
 
-    if (l === r) {
-        return lists[l]
-    }
-    
-    // let m = Math.floor((l+r)/2)
-    let m = Math.floor(l + (r-l)/2)
+  if (l === r) {
+    return lists[l];
+  }
 
-    let left = divide(lists, l, m)
-    let right = divide(lists, m + 1, r)
+  // let m = Math.floor((l+r)/2)
+  let m = Math.floor(l + (r - l) / 2);
 
-    return conquer(left, right)
+  let left = divide(lists, l, m);
+  let right = divide(lists, m + 1, r);
 
+  return conquer(left, right);
 }
 
 /**
-* @param {ListNode} l1
-* @param  {ListNode} l2
-* @return {ListNode}
-*/
+ * @param {ListNode} l1
+ * @param  {ListNode} l2
+ * @return {ListNode}
+ */
 function conquer(l1, l2) {
-    const dummy = new ListNode(0)
-    let curr = dummy
+  const dummy = new ListNode(0);
+  let curr = dummy;
 
-    while (l1 && l2) {
-        if (l1.val <= l2.val) {
-            curr.next = l1
-            l1 = l1.next
-        } else {
-            curr.next = l2
-            l2 = l2.next
-        }
-        curr = curr.next
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
     }
+    curr = curr.next;
+  }
 
-    // 남은 linked-list는 그대로 붙이기
-    curr.next = l1 ? l1 : l2 
+  // 남은 linked-list는 그대로 붙이기
+  curr.next = l1 ? l1 : l2;
 
-    return dummy.next
-
+  return dummy.next;
 }
