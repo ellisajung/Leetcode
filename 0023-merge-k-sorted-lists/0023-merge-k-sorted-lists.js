85a8036d39650dc0569dcdba1e9f643520d03db9
Elisa <ellisajung12@gmail.com>
2025-02-22 14:26:26 +0900

    Time: 6 ms (88.25%), Space: 61.5 MB (5.58%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -9,6 +9,13 @@
 //       [1,4,5]       [1,3,4]      [2,6]      [5,6,7] [3,4]
 //                                                  /       \
 //                                            [5,6,7]        [3,4]
+
+//           [5 -> 6 -> 7]
+//  dummy        \      l1
+//        \  [3 -> 4]
+//               l2
+//                c   
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -21,49 +28,62 @@
  * @return {ListNode}
  */
 function mergeKLists (lists) {
-        if (!lists || lists.length === 0) {
-            return null;
-        }
-        return divide(lists, 0, lists.length - 1);
+    // Edge cases
+    if (!lists || lists.length === 0) { // lists가 null이거나 빈 배열일 경우
+        return null
     }
 
-    /**
-     * @param {ListNode[]} lists
-     * @param {number} l
-     * @param {number} r
-     * @return {ListNode}
-     */
-    const divide = (lists, l, r) => {
-        if (l > r) {
-            return null;
-        }
-        if (l === r) {
-            return lists[l];
-        }
-        const mid = Math.floor(l + (r - l) / 2);
-        const left = divide(lists, l, mid);
-        const right = divide(lists, mid + 1, r);
-        return conquer(left, right);
+    return divide(lists, 0, lists.length - 1)
+}
+
+/**
+* @param {ListNode[]} lists
+* @param {number} l
+* @param {number} r
+* @return {ListNode}
+*/
+function divide(lists, l, r) {
+    if (l > r) { // 이 조건은 왜 있어야 되는거임?
+        return null
     }
 
-    /**
-     * @param {ListNode} l1
-     * @param  {ListNode} l2
-     * @return {ListNode}
-     */
-    const conquer = (l1, l2) => {
-        const dummy = new ListNode(0);
-        let curr = dummy;
-        while (l1 && l2) {
-            if (l1.val <= l2.val) {
-                curr.next = l1;
-                l1 = l1.next;
-            } else {
-                curr.next = l2;
-                l2 = l2.next;
-            }
-            curr = curr.next;
-        }
-        curr.next = l1 ? l1 : l2;
-        return dummy.next;
+    if (l === r) {
+        return lists[l]
     }
+    
+    // let m = Math.floor((l+r)/2)
+    let m = Math.floor(l + (r-l)/2)
+
+    let left = divide(lists, l, m)
+    let right = divide(lists, m + 1, r)
+
+    return conquer(left, right)
+
+}
+
+/**
+* @param {ListNode} l1
+* @param  {ListNode} l2
+* @return {ListNode}
+*/
+function conquer(l1, l2) {
+    const dummy = new ListNode(0)
+    let curr = dummy
+
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            curr.next = l1
+            l1 = l1.next
+        } else {
+            curr.next = l2
+            l2 = l2.next
+        }
+        curr = curr.next
+    }
+
+    // 남은 linked-list는 그대로 붙이기
+    curr.next = l1 ? l1 : l2 
+
+    return dummy.next
+
+}
